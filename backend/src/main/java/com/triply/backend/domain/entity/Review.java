883eb116f531cb7a5b660a07d2f59bf86cb43b55
Jakub <jakub.gorzkowski@student.pk.edu.pl>
2024-10-21 00:05:15 +0200

    Setup database

---
+++ backend/src/main/java/com/triply/backend/domain/entity/Review.java
@@ -0,0 +1,37 @@
+package com.triply.backend.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    private Long id;
+
+    private Byte rating;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id", referencedColumnName = "id")
+    private Place place;
+}
