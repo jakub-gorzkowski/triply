f3a801dd0da0921213d898be3142501bf7c06513
Jakub <jakub.gorzkowski@student.pk.edu.pl>
2024-10-27 23:47:09 +0100

    Setup JWT authentication configuration

--- backend/src/main/java/com/triply/backend/domain/entity/User.java
+++ backend/src/main/java/com/triply/backend/domain/entity/User.java
@@ -1,6 +1,10 @@
 package com.triply.backend.domain.entity;
 
+import com.triply.backend.security.enumeration.Role;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
@@ -10,8 +14,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -21,9 +30,10 @@ import java.util.Set;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
+    @GeneratedValue
     private Long id;
 
     private String email;
@@ -31,6 +41,9 @@ public class User {
     private String username;
 
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @OneToOne(mappedBy = "user")
     private ToDoList toDoList;
@@ -40,4 +53,39 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private Set<Review> reviewSet = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ backend/src/main/java/com/triply/backend/security/enumeration/Role.java
@@ -0,0 +1,6 @@
+package com.triply.backend.security.enumeration;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER
+}
